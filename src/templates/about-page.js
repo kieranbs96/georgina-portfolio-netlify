3b25a3b21492d79eff6a5894918937f4908f20a1
kieranbs96 <kieranbs96@gmail.com>
2018-04-22 15:56:42 +0100

    Major changes

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -2,24 +2,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Content, { HTMLContent } from '../components/Content'
 
+import Collapsible from 'react-collapsible';
+import rehypeReact from 'rehype-react';
+import Link from 'gatsby-link';
+
 export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content
 
   return (
     <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
-              </h2>
-              <PageContent className="content" content={content} />
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+      <h1 className="title is-size-3 has-text-weight-bold is-bold-light">
+        {title}
+      </h1>
+      <PageContent content={content} />
+    </section >
   )
 }
 
@@ -31,12 +27,16 @@ AboutPageTemplate.propTypes = {
 
 const AboutPage = ({ data }) => {
   const { markdownRemark: post } = data
+  const renderAst = new rehypeReact({
+    createElement: React.createElement,
+    components: { 'react-collapsible': Collapsible, 'react-link': Link },
+  }).Compiler;
 
   return (
     <AboutPageTemplate
       contentComponent={HTMLContent}
       title={post.frontmatter.title}
-      content={post.html}
+      content={renderAst(post.htmlAst)}
     />
   )
 }
@@ -50,7 +50,7 @@ export default AboutPage
 export const aboutPageQuery = graphql`
   query AboutPage($id: String!) {
     markdownRemark(id: { eq: $id }) {
-      html
+      htmlAst
       frontmatter {
         title
       }
